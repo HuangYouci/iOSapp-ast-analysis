012c744b65b01cc19734a8d46a85aee3e40bd7b1
Hunag Youci <huangyouci@icloud.com>
2025-03-21 16:44:28 +0800

    Initial Commit

---
+++ ast-analysis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ast-analysis
+//
+//  Created by 黃宥琦 on 2025/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ast-analysis/ast_analysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ast_analysisApp.swift
+//  ast-analysis
+//
+//  Created by 黃宥琦 on 2025/3/21.
+//
+
+import SwiftUI
+
+@main
+struct ast_analysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
